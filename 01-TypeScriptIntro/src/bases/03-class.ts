e85e9631e9f786720f5d924ce79f6dd2d8c49453
Matias <matiasluduenia@gmail.com>
2023-05-10 12:43:22 -0300

    clase20

--- 01-TypeScriptIntro/src/bases/03-class.ts
+++ 01-TypeScriptIntro/src/bases/03-class.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { Move, Pokeapiresponse } from '../interfaces/pokeapi-response.interface';
 export class Pokemon{
 
 
@@ -16,8 +18,15 @@ get imageUrl():string{
     speak(){
         console.log(`${this.name},${this.name}`)
     }
+    async getMoves(): Promise<Move[]>{
+        10;
+        const { data }= await axios.get<Pokeapiresponse>('http://pokeapi.co/api/v2/pokemon/4');
+        const moves=data.moves;
+        return moves;
+    }
 }
 export const charmander= new Pokemon(1,'charmander')
 
-charmander.speak();
-charmander.scream();
+// charmander.speak();
+// charmander.scream()
+charmander.getMoves();
--- 01-TypeScriptIntro/src/main.ts
+++ 01-TypeScriptIntro/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { charmander } from './bases/03-class.ts'
+import { charmander } from './bases/04-Injection.ts'
 // import { pokemons } from './bases/02-objects';
 
 
